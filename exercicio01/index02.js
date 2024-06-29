54328ca67f34d978e69fffa2a3d7fce2e3f7ef6f
Danilo Marques <marquesdanilo688@gmail.com>
2024-06-28 22:00:28 -0300

    update index02.js

--- exercicio01/index02.js
+++ exercicio01/index02.js
@@ -2,4 +2,8 @@ const prompt = require('prompt-sync')();
 
 const numero = Number(prompt("Informe um número: "))
 
-if()
+if(numero %2 === 0){
+    console.log("O numero informado é par")
+}else{
+    console.log("O numero infordo é impar")
+}
