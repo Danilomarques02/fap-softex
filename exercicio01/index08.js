b1b66bf90d9d7e87d763a9af53e48465d1f973e5
Danilo Marques <marquesdanilo688@gmail.com>
2024-06-29 17:01:16 -0300

    create

---
+++ exercicio01/index08.js
@@ -0,0 +1,25 @@
+const prompt = require('prompt-sync')();
+
+const numero1 = Number(prompt("Informe um numero: "))
+const numero2 = Number(prompt("Infome um segundo numero "))
+const operacao = prompt("informe a operação que deseja(+,-,/,*): ")
+
+let resultado;
+
+if(operacao === '+'){
+    resultado = numero1 + numero2
+  console.log(`O resultado da sua soma é ${resultado}`)
+} 
+else if(operacao === '-'){
+    resultado = numero1 - numero2
+    console.log(`O resultado da sua subtração é ${resultado}`)
+}
+else if(operacao === '/'){
+    resultado = numero1 / numero2
+    console.log(`O resultado da sua divisão é ${resultado}`)
+    }
+    else{
+        resultado = numero1 * numero2
+        console.log(`O resultado da sua multiplicação é ${resultado}`)
+
+    }
